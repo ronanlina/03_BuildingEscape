4bc61fe1b7c9004c029dfdf76f3e4cfce619e9a3
Ronan Lina <ronanlina@gmail.com>
2018-05-04 01:54:53 +0800

    S3 Wrap Up - Added requisites for elevator mechanics

---
+++ BuildingEscape/Source/BuildingEscape/Elevator.h
@@ -0,0 +1,30 @@
+// Copyright Ronan Lina
+
+#pragma once
+
+#include "CoreMinimal.h"
+#include "Components/ActorComponent.h"
+#include "Engine/TriggerVolume.h"
+#include "Elevator.generated.h"
+
+
+UCLASS( ClassGroup=(Custom), meta=(BlueprintSpawnableComponent) )
+class BUILDINGESCAPE_API UElevator : public UActorComponent
+{
+	GENERATED_BODY()
+
+public:	
+	// Sets default values for this component's properties
+	UElevator();
+
+protected:
+	// Called when the game starts
+	virtual void BeginPlay() override;
+
+public:	
+	// Called every frame
+	virtual void TickComponent(float DeltaTime, ELevelTick TickType, FActorComponentTickFunction* ThisTickFunction) override;
+
+private:
+	
+};
